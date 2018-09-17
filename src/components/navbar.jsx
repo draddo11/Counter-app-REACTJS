871bb9b540d25209f53ad73ed1687c861bb017b9
Onesimus Addo Appiah <38097150+draddo11@users.noreply.github.com>
2018-09-17 22:20:45 +0100

    changed navbar title
    changed navbar title from Navbar to counter with react js

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,7 +5,7 @@ const NavBar = ({totalCounters})=> {
      return ( 
             <nav className="navbar navbar-light bg-light">
               <a className="navbar-brand" href="#">
-              Navbar{" "} 
+              Counter with React{" "} 
               <span className="badge badge-pill badge-secondary">
               {totalCounters}
               </span>
